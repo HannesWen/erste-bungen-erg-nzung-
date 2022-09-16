53495ed1dfa9bf732317dab2a5c10ce07896fb68
Wengert, Johannes <johannes.wengert@tecalliance.net>
2022-09-16 12:01:15 +0200

    Taschenrechner mit Methoden

--- Uebungen (ergänzung)/ConsoleApp1/Program.cs
+++ Uebungen (ergänzung)/ConsoleApp1/Program.cs
@@ -10,23 +10,59 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Wir holen uns jetzt eine Zahl aus Number");
-            List<string> names = Number();
-            foreach (var name in names)
-            {
-                Console.WriteLine(name);
-            }
+            Console.WriteLine("Gib mir bitte zwei Zahlen()");
+            double zahl1 = Convert.ToDouble.(Console.ReadLine());
+            double zahl2 = Convert.ToDouble.(Console.ReadLine());
+
+            double ergebnis = Add(zahl1, zahl2);
+            Console.WriteLine("Das Ergebnis der addition lautet " + ergebnis);
+
+            double ergebnis1 = Multiply(zahl1, zahl2);
+            Console.WriteLine("Das Ergebnis der multiplikation lautet " + ergebnis1);
+
+            double ergebnis2 = subtract(zahl1, zahl2);
+            Console.WriteLine("Das Ergebnis der subtraktion lautet " + ergebnis2);
+
+            double ergebnis3 = divide(zahl1, zahl2);
+            Console.WriteLine("Das Ergebnis der division lautet " + ergebnis3);
+
             Console.ReadKey();
         }
 
-        //static Rückgabetyp MethodenName (Parameter)
-        static List<string> Number()
+        static double Add(double z1, double z2)
         {
-            List<string> name = new List<string>();
-            name.Add("ABC");
-
-            //Rückgabevariable
-            return name;
+            double summe = z1 + z2;
+            return summe;
         }
+
+        static double Multiply(double z1, double z2)
+        {
+            double summe = z1 * z2;
+            return summe;
+        }
+
+        static double subtract(double z1, double z2)
+        {
+            double summe = z1 - z2;
+            return summe;
+        }
+
+        static double divide(double z1, double z2)
+        {
+            double summe = z1 / z2;
+            return summe;
+        }
+
+
+
+
+        ////static Rückgabetyp MethodenName (Parameter)
+        //static List<string> Number()
+        //{
+        //    List<string> name = new List<string>();
+        //    name.Add("ABC");
+
+        //    //Rückgabevariable
+        //    return name;
     }
 }

73ec71dd895ca49d2b840053c0e9d78dae7e1c7f
Wengert, Johannes <johannes.wengert@tecalliance.net>
2022-09-16 11:12:07 +0200

    Liste befüllen und ausgeben mit Variabler anzahl

--- Uebungen (ergänzung)/ConsoleApp1/Program.cs
+++ Uebungen (ergänzung)/ConsoleApp1/Program.cs
@@ -10,91 +10,23 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            ////Seite 2, Aufgabe 5:
-            //for (int i = 0; i < 100; i++)
-            //{
-            //    if (i % 3 == 0 && i % 5 == 0)
-            //    {
-            //        Console.WriteLine("FizzBuzz");
-            //    }
-            //    else if (i % 3 == 0)
-            //    {
-            //        Console.WriteLine("Fizz");
-            //    }
-            //    else if (i % 5 == 0)
-            //    {
-            //        Console.WriteLine("Buzz");
-            //    }
-            //    else
-            //    {
-            //        Console.WriteLine(i);
-            //    }
-            //}
-            //Console.ReadKey();
-
-
-            //Switch
-            //string zahl = Console.ReadLine();
-            //switch(zahl)
-            //{
-            //    case "1":
-            //        Console.WriteLine("Die Zahl ist 1");
-            //        break;
-            //    case "2":
-            //        Console.WriteLine("Die Zahl ist 2");
-            //        break;
-            //    case "3":
-            //        Console.WriteLine("Die Zahl ist fünf");
-            //        break;
-            //    default:
-            //        Console.WriteLine("Kein Fall trifft zu");
-            //        break;
-            //}
-            //Console.ReadKey();
-
-            //Taschenrechner simpel
-
-            //Console.WriteLine("Gebe mir eine Zahl");
-            //int zahl1 = Convert.ToInt32(Console.ReadLine());
-
-            //Console.WriteLine("Was möchtest du mit der Zahl machen?");
-            //string rechenzeichen = (Console.ReadLine());
-
-            //Console.WriteLine("Gebe mir eine weitere Zahl");
-            //int zahl2 = Convert.ToInt32(Console.ReadLine());
-
-            //double ergebnis = 0;
-
-            //switch (rechenzeichen)
-            //{
-            //    case "*":
-            //        ergebnis = zahl1 * zahl2;
-            //        Console.WriteLine(ergebnis);
-            //        break;
-            //    case "/":
-            //        ergebnis = zahl1 / zahl2;
-            //        Console.WriteLine(ergebnis);
-            //        break;
-            //    case "+":
-            //        ergebnis = zahl1 + zahl2;
-            //        Console.WriteLine(ergebnis);
-            //        break;
-            //    case "-":
-            //        ergebnis = zahl1 - zahl2;
-            //        Console.WriteLine(ergebnis);
-            //        break;
-            //}
-            //Console.ReadKey();
-
-
             // Listen
-            //List<int> numbers = new List<int>();
-            List<string> names = new List<string>();
-            names.Add("Christian");
-            names.Add("Lukas");
-            names.Add("Andreas");
-            names.Add("Robert")           
+            Console.WriteLine("Bitte gib mir einen Namen, wenn deine Liste fertig sein soll schreibe - ");
+            List<string> list = new List<string>();
+            string eingabe = "0";
 
+            do
+            {
+                eingabe = Console.ReadLine();
+                list.Add(eingabe);
+            } while (eingabe != "-");
+
+            Console.WriteLine(list);
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
             Console.ReadKey();
         }
     }

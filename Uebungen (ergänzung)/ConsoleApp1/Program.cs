d0a33524860146bc44937da8b2e4615442696527
Wengert, Johannes <johannes.wengert@tecalliance.net>
2022-09-19 08:40:56 +0200

    Aufgaben Wiederholung Tag 1, Seite 4, Aufgabe 1

--- Uebungen (ergänzung)/ConsoleApp1/Program.cs
+++ Uebungen (ergänzung)/ConsoleApp1/Program.cs
@@ -4,6 +4,7 @@ using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Net.Configuration;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConsoleApp1
@@ -45,51 +46,63 @@ namespace ConsoleApp1
             //Console.ReadKey();
 
             //Roulette
-            Random r = new Random();
-            int dollar = 10000;
-            int einsatz = 10;
-            int runden = 0;
-            do
+            //Random r = new Random();
+            //int startwert = 10000;
+            //int dollar = 10000;
+            //int einsatz = 10;
+            //int runden = 0;
+            //do
+            //{
+            //    int k = r.Next(0, 37);
+            //    Console.WriteLine($"Du hast vor dem Spiel: {dollar} dollar.");
+            //    Console.WriteLine($"Runde {runden}.");
+            //    if (einsatz > dollar)
+            //    {
+            //        Console.WriteLine($"Dein neuer einsatz {einsatz} ist höher als deine dollar {dollar}.");
+            //        Console.WriteLine($"Du setzt nun all deine verbliebenen dollar {dollar}.");
+            //        einsatz = dollar;
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"Dein einsatz ist {einsatz}");
+            //    }
+            //    Console.WriteLine($"Die geworfene Zahl ist: {k}.");
+            //    if (k % 2 != 0)
+            //    {
+            //        Console.WriteLine($"Du lagst falsch. Der Einsatz {einsatz} wird dir abgebucht.");
+            //        dollar = dollar - einsatz;
+            //        einsatz = einsatz * 2;
+            //        if (einsatz >= 1000)
+            //        {
+            //            einsatz = 1000;
+            //        }
+            //    }
+            //    else if ((k % 2 == 0) && k > 0)
+            //    {
+            //        Console.WriteLine($"Du lagst richtig. Der Einsatz {einsatz} wird dir gutgeschrieben."); 
+            //        dollar = dollar + einsatz;
+            //        if (dollar >= 10000)
+            //        {
+            //            einsatz = 10;
+            //        }
+            //    }
+            //    Console.WriteLine($"Du hast nach dem Spiel: {dollar} dollar.");
+            //    Console.WriteLine();
+            //    runden = runden + 1;
+            //} while (dollar > 0);
+            //Console.WriteLine($"Das Spiel ist aus. Du hast {runden} runden gespielt und all dein Geld verloren.");
+            //Console.WriteLine("Da siehst du mal was Glücksspiel anrichten kann. Infos und Hilfe unter www.bzga.de."); 
+            //Console.ReadKey();
+
+            // "Aufgaben Wiederholung Tag 1", Seite 4, Aufgabe 1
+            List<int> numbers = new List<int>();
+            numbers.Add(5);
+            numbers.Add(3);
+            numbers.Add(7);
+            foreach (int i in numbers)
             {
-                int k = r.Next(0, 36);
-                Console.WriteLine($"Du hast vor dem Spiel: {dollar} dollar.");
-                Console.WriteLine($"Runde {runden}.");
-                if (einsatz > dollar)
-                {
-                    Console.WriteLine($"Dein neuer einsatz {einsatz} ist höher als deine dollar {dollar}.");
-                    Console.WriteLine($"Du setzt nun all deine verbliebenen dollar {dollar}.");
-                    einsatz = dollar;
-                }
-                else
-                {
-                    Console.WriteLine($"Dein einsatz ist {einsatz}");
-                }
-                Console.WriteLine($"Die geworfene Zahl ist: {k}.");
-                if (k % 2 != 0)
-                {
-                    Console.WriteLine($"Du lagst falsch. Der Einsatz {einsatz} wird dir abgebucht.");
-                    dollar = dollar - einsatz;
-                    einsatz = einsatz * 2;
-                    if (einsatz >= 1000)
-                    {
-                        einsatz = 1000;
-                    }
-                }
-                else if ((k % 2 == 0) && k > 0)
-                {
-                    Console.WriteLine($"Du lagst richtig. Der Einsatz {einsatz} wird dir gutgeschrieben."); 
-                    dollar = dollar + einsatz;
-                    if (dollar >= 10000)
-                    {
-                        einsatz = 10;
-                    }
-                }
-                Console.WriteLine($"Du hast nach dem Spiel: {dollar} dollar.");
-                Console.WriteLine();
-                runden = runden + 1;
-            } while (dollar > 0);
-            Console.WriteLine($"Das Spiel ist aus. Du hast {runden} runden gespielt und all dein Geld verloren.");
-            Console.WriteLine("Da siehst du mal was Glücksspiel anrichten kann. Infos und Hilfe unter www.bzga.de."); 
+                Console.WriteLine(i);
+            }
             Console.ReadKey();
         }
     }

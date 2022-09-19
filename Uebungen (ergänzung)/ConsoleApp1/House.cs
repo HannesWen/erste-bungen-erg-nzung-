d824ca1d5a8dce4bff88dd6dad897f8fb45edc98
Wengert, Johannes <johannes.wengert@tecalliance.net>
2022-09-19 13:32:27 +0200

    no message

---
+++ Uebungen (ergänzung)/ConsoleApp1/House.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class House
+    {
+        public double Length_house { get; set; }
+        public double Width_house { get; set; }
+        public double Height { get; set; }
+        public string Roof { get; set; }
+        public string Color{ get; set; }
+        public int Floor { get; set; }
+        public double Price { get; set; }
+        public double Length_property { get; set; }
+        public double Width_property { get; set; }
+        public string Colorchange { get; set; }
+
+
+        public House(double length_house, double width_house, double height, string roof, string color, int floor, double price, double length_property, double width_property)
+        {
+            Length_house = length_house;
+            Width_house = width_house;
+            Height = height;
+            Roof = roof; 
+            Color = color;
+            Floor = floor; 
+            Price = price; 
+            Length_property = length_property;
+            Width_property = width_property;
+        }
+        public static House HouseData()
+        {
+            double length_house = Convert.ToDouble(Console.ReadLine());
+            double width_house = Convert.ToDouble(Console.ReadLine());
+            double height = Convert.ToDouble(Console.ReadLine());
+            string roof = Console.ReadLine();
+            string color = Console.ReadLine();
+            int floor = Convert.ToInt32(Console.ReadLine());
+            double price = Convert.ToDouble(Console.ReadLine());
+            double length_property = Convert.ToDouble(Console.ReadLine());
+            double width_property = Convert.ToDouble(Console.ReadLine());
+
+            House h1 = new House(length_house, width_house, height, roof, color, floor, price, length_property, width_property);
+            return h1;
+        }
+
+        public void HouseInformation()
+        {
+            Console.WriteLine($"Dein Haus hat folgende Daten: Length_house = {Length_house} Width_house = {Width_house}" +
+                $" Height = {Height} Roof = {Roof} Floor = {Floor} Price = {Price} Length_property = {Length_property} " +
+                $" Width_property = {Width_property}");
+        }
+        public void GrundstückQM()
+        {
+            double propertyqm = Length_property * Width_property;
+            Console.WriteLine($"Dein Grundstück ist {propertyqm} groß");
+        }
+        public void Hausfläche()
+        {
+            double livingarea = Length_house * Width_house * Floor;
+            Console.WriteLine($"Dein Haus ist {livingarea} groß");
+        }
+        public void Hausverputzen(string color)
+        {
+            Color = color;
+            Console.WriteLine($"Deine neue Hausfarbe ist {color}");
+        }
+    }
+}
--- Uebungen (ergänzung)/ConsoleApp1/Program.cs
+++ Uebungen (ergänzung)/ConsoleApp1/Program.cs
@@ -15,58 +15,45 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            //Car c1 = new Car(12345, "Audi", "A3", "2.0 TDI", 1980, "Red", 4);
-            //c1.PrintCarInformation();
 
-            //Car c2 = new Car(25642, "BMW", "3", "2.2", 2200, "Black", 4);
+            // Der User gibt uns Daten für X Häuser welche wir wiedergeben.
+            // Erstelle eine Klasse Huas.
+            // Die Häuser haben folgende Eigenschaften
+            // Laenge Haus flieskommazahl, Breite Haus flieskommazahl, Laenge Grundstück flieskommazahl, Breite Grundstück flieskommazahl, Höhe flieskommazahl, Dachart string, Putzfarbe string, Etagen int, Preis flieskommazahl
 
-            //c2.PrintCarInformation();
+            // Methoden
+            // Grundstück QM
+            // Hauswohlfläcke (länge * breite + etage)
+            // Hausverputzen (Farbe des putzes Ändern)
+            // PrintEigenschaften
 
-            //List<Car> cars = new List<Car>();
-
-            //foreach (Car car in cars)
-            //{
-            //    car.PrintCarInformation();
-            //}
-
-            //Console.ReadLine();
-
-
-            // Frage den User nach einer beliebigen Anzahl von Fahrzeugsätzen und lass sie wieder ausgeben.
-
-            Console.WriteLine("Wie viele Fahrzeuge möchtest du anlegen?");
+            Console.WriteLine("Wie viele Häuser möchtest du anlegen?");
             int anzahl = Convert.ToInt32(Console.ReadLine());
 
-            List<Car> fahrzeugdaten = new List<Car>();
+            List<House> housedata = new List<House>();
 
             for (int i = 0; i < anzahl; i++)
             {
-                Console.WriteLine("Bitte gib mir folgende Fahrzeugdaten: TypeId, Make, Model, TypeName, Ccm, Color, Tyres");
-
-                fahrzeugdaten.Add(Car.CarData());
+                Console.WriteLine("Bitte gib mir folgende Hausdaten: length_house, width_house, height, roof, color, floor, price, length_property, width_property ");
+                housedata.Add(House.HouseData());
             }
-            foreach (Car c in fahrzeugdaten)
+            foreach (House h in housedata)
             {
-                c.PrintCarInformation();
+                h.HouseInformation();
+                h.GrundstückQM();
+                h.Hausfläche();
+            }
+            Console.WriteLine("Möchtest du die Farbe eines Hauses Ändern ja / nein ?");
+            string ändern = Console.ReadLine();
+            if (ändern == "ja")
+            {
+                Console.WriteLine("Bei welchem Haus möchtest du die Farbe ändern ?");
+                int welcheshaus = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Welche Farbe soll es haben ?");
+                string color = Console.ReadLine();
+                housedata[welcheshaus].Hausverputzen(Console.ReadLine());
             }
             Console.ReadKey();
-
-
-            //List<Cars> cars = new List<Car>();
-            //for (int i = 0; i < 3; i++)
-            //{
-            //    cars.Add(Car.AskUserForCar());
-            //}
-
-            //foreach (Car car in cars)
-            //{
-            //    car.PrintCarInformation();
-            //}
-            //Console.ReadKey();
-
-
-
-
         }
     }
 }
